879d1b2d9152f8e63e859b15130e3fe102c47585
Megan Coyle <megan@mcoyle.com>
2020-06-13 00:47:20 -0400

    Add pagination

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import DirectoryTable from "./components/DirectoryTable";
 import AddUserForm from "./components/AddUserForm";
 import EditUserForm from "./components/EditUserForm";
+import Pagination from "./components/Pagination";
+
+const dummyData = [
+  { id: 1, first_name: "Ann", last_name: "Smith", title: "Dev" },
+  { id: 2, first_name: "Shawn", last_name: "Williams", title: "Analyst" },
+  { id: 3, first_name: "Sarah", last_name: "Jones", title: "Teller" },
+  { id: 4, first_name: "Ann", last_name: "Smith", title: "Dev" },
+  { id: 5, first_name: "Shawn", last_name: "Williams", title: "Analyst" },
+  { id: 6, first_name: "Sarah", last_name: "Jones", title: "Teller" },
+  { id: 7, first_name: "Ann", last_name: "Smith", title: "Dev" },
+  { id: 8, first_name: "Shawn", last_name: "Williams", title: "Analyst" },
+  { id: 9, first_name: "Sarah", last_name: "Jones", title: "Teller" },
+  { id: 10, first_name: "Ann", last_name: "Smith", title: "Dev" },
+  { id: 11, first_name: "Shawn", last_name: "Williams", title: "Analyst" },
+  { id: 12, first_name: "Sarah", last_name: "Jones", title: "Teller" },
+];
 
 const App = () => {
-  const dummyData = [
-    { id: 1, first_name: "Ann", last_name: "Smith", title: "Dev" },
-    { id: 2, first_name: "Shawn", last_name: "Williams", title: "Analyst" },
-    { id: 3, first_name: "Sarah", last_name: "Jones", title: "Teller" },
-  ];
-
   const [users, setUsers] = useState(dummyData);
   const [editing, setEditing] = useState(false);
   const initialFormState = {
@@ -19,6 +29,9 @@ const App = () => {
     title: "",
   };
   const [currentUser, setCurrentUser] = useState(initialFormState);
+  // const [loading, setLoading] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [usersPerPage] = useState(10);
 
   // incrementing ids manually since this uses hardcoded dummy data
   // TODO: update this when tying it to an API/database
@@ -47,6 +60,13 @@ const App = () => {
     setUsers(users.filter((user) => user.id !== id));
   };
 
+  // pagination
+  const indexOfLastUser = currentPage * usersPerPage;
+  const indexOfFirstUser = indexOfLastUser - usersPerPage;
+  const currentUsers = users.slice(indexOfFirstUser, indexOfLastUser);
+  // change page
+  const paginate = (pageNumber) => setCurrentPage(pageNumber);
+
   return (
     <div className="container">
       <h1>Employee Directory</h1>
@@ -65,9 +85,14 @@ const App = () => {
         </>
       )}
       <DirectoryTable
-        users={users}
+        users={currentUsers}
         editUser={editUser}
         deleteUser={deleteUser}
+      />
+      <Pagination
+        usersPerPage={usersPerPage}
+        totalUsers={users.length}
+        paginate={paginate}
       />
     </div>
   );
